4ee700d88dc27ba4518caea32c94c4053aa025b0
Graham-R7 <Ryan.graham3@ucalgary.ca>
2025-03-14 11:21:25 -0600

    Finished until pg10

--- Lab07/tic-tac-toe/src/App.js
+++ Lab07/tic-tac-toe/src/App.js
@@ -1,39 +1,40 @@
 import './App.css';
+import { useState } from "react"; 
 
-function App() {
-  return (
-    <Board />
-  );
-}
+function Square() { 
+  const [value, setValue] = useState(null); 
+ 
+  function handleClick() { 
+    setValue('X'); 
+  } 
+  return ( 
+    <button 
+      className="square" 
+      onClick={handleClick} 
+    > 
+      {value} 
+    </button> 
+  ); 
+} 
 
-export default App;
-
-function Square({ value }) { 
-  return (
-    <button className="square">
-      {value}
-    </button>
-  );
-}
-
-export function Board() {
-  return (
-    <>
-      <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
-      </div>
-      <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
-      </div>
-      <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
-      </div>
-    </>
-  );
+export default function Board() { 
+  return ( 
+    <> 
+      <div className="board-row"> 
+        <Square /> 
+        <Square /> 
+        <Square /> 
+      </div> 
+      <div className="board-row"> 
+        <Square /> 
+        <Square /> 
+        <Square /> 
+      </div> 
+      <div className="board-row"> 
+        <Square /> 
+        <Square /> 
+        <Square /> 
+      </div> 
+    </> 
+  ); 
 }
