28d02218ae82196b4f4e71a98b83e4c9b5c9d85b
officialRababRahman <rababrahman04@gmail.com>
2025-03-28 12:55:34 -0600

    install react-router-dom

--- Lab09/HousePricePrediction/frontend/src/App.js
+++ Lab09/HousePricePrediction/frontend/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './Login';
 import HousePricePredictor from './HousePricePredictor';
 
 const App = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/predict" element={<HousePricePredictor />} />
+      <Route path="/" element={<Navigate to="/login" />} />
     </Routes>
   </BrowserRouter>
 );
