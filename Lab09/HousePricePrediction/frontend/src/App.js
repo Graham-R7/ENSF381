66ae55176ae61220b0020278fcc9523a298d71cb
Graham-R7 <Ryan.graham3@ucalgary.ca>
2025-03-28 12:49:42 -0600

    Again?

--- Lab09/HousePricePrediction/frontend/src/App.js
+++ Lab09/HousePricePrediction/frontend/src/App.js
@@ -11,28 +11,5 @@ const App = () => (
     </Routes>
   </BrowserRouter>
 );
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import Login from "./Login";
-import "./App.css";
-
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          {/* ...existing routes... */}
-          <Route path="/" element={<Navigate to="/login" />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-}
 
 export default App;
