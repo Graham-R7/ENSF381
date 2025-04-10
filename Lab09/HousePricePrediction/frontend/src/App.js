af3daa22c5b0a38360056a0307bd5e0d1e1cc9cb
officialRababRahman <rababrahman04@gmail.com>
2025-04-10 17:48:13 -0600

    update app.js

--- Lab09/HousePricePrediction/frontend/src/App.js
+++ Lab09/HousePricePrediction/frontend/src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Login';
 import HousePricePredictor from './HousePricePredictor';
 
 const App = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Login />} />
       <Route path="/predict" element={<HousePricePredictor />} />
-      <Route path="/" element={<Navigate to="/login" />} />
     </Routes>
   </BrowserRouter>
 );
